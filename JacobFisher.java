5e0dfcaf7f55b1ab56aa68523d9fe6293a0bd68a
fisherjacobc <72705185+fisherjacobc@users.noreply.github.com>
2026-09-18 16:57:49 -0400

    remove placeholder java files

--- JacobFisher.java
+++
@@ -1,5 +0,0 @@
-public class JacobFisher {
-    public static void main(String[] args) {
-        System.out.println("Jacob Fisher is on the team.");
-    }
-}
--- JonathanDavis.java
+++
@@ -1,5 +0,0 @@
-public class JonathanDavis {
-    public static void main(String[] args) {
-        System.out.println("Jonathan Davis is on the team.");
-    }
-}
